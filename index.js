35ddd16513107e56ab4484741bcf30e63f3401d5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-06 21:14:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector)
+
+}
+
+function nestedTarget() {
+  return document.querySelector( 'div#nested div.target' )
+}
+
+
+function increaseRankBy(n) {
+var lis = document.querySelectorAll('ul.ranked-list li')
+  for (var i=0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
