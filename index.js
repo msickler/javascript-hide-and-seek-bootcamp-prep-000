33fe7342482e44ba06fe4278c4a5718bcb8d8400
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-06 21:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function nestedTarget() {
 function increaseRankBy(n) {
   return  parseInt(document.querySelectorAll('ul.ranked-list')+ 'n')
 }
+function increaseRankBy(n) {
+  lis = document.querySelectorAll('ul.ranked-list')
+  for (let i=0; i < lis.length; i++) {
+    list[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+    return lis
+  }
+}
