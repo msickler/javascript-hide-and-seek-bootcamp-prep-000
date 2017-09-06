c6a1498a7e3cdc52f4aee72e02c987433ffae8bf
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-06 21:10:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
 var lis = document.querySelectorAll('ul.ranked-list li')
-  for (let i=0; i < lis.length; i++) {
+  for (var i=0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
     return lis
   }
