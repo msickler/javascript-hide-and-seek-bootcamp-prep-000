105e55923119466b9cdb2fa89067a25ec0b50cbb
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-06 21:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,5 @@ function increaseRankBy(n) {
 var lis = document.querySelectorAll('ul.ranked-list li')
   for (var i=0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
-    return lis
   }
 }
